f0228abb0852270c4c10067741dc0b24d2166b09
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-30 06:51:08 +0530

    disable_deletion_protection.
    Because not working in make dev-destroy

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_lb" "main" {
   security_groups    = [aws_security_group.sg.id]
   subnets            = var.subnets
 
-  #enable_deletion_protection = true
+  disable_deletion_protection = true
 
   tags       = merge({ Name = "${var.name}-${var.env}" }, var.tags)
 }
