507eae09640598cf08c5a6366691099d6f32cf8e
Paul Prem Kumar <paul.prem5@gmail.com>
2024-02-09 03:31:49 +0530

    8080-80 in frontend, 80,443

--- variables.tf
+++ variables.tf
@@ -3,9 +3,6 @@ variable "internal" {}
 variable "load_balancer_type" {}
 variable "env" {}
 variable "vpc_id" {}
-variable "port" {
-  default = 443
-}
 variable "sg_subnets_cidr" {}
 variable "subnets" {}
 variable "tags" {}
